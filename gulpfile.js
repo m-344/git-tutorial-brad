521c3dc7c5ab30cc41ca6a324039b4a1abddce02
m-344 <le.chat454@gmail.com>
2019-09-11 16:38:34 +0200

    add gulpfile.js

---
+++ gulpfile.js
@@ -0,0 +1,13 @@
+var gulp = require('gulp'),
+watch = require('gulp-watch');
+
+gulp.task('styles', function () {
+	console.log('works');
+});
+
+gulp.task('watch', function () {
+	
+	watch('./styles/**/*..css', function () {
+		gulp.start('styles');
+	})
+});
