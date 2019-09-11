d35c2423af3aa441abc37004b5ee14aad87e0d39
m-344 <le.chat454@gmail.com>
2019-09-11 18:46:06 +0200

    add postcss pipe

--- gulpfile.js
+++ gulpfile.js
@@ -1,8 +1,13 @@
 var gulp = require('gulp'),
-watch = require('gulp-watch');
+watch = require('gulp-watch'),
+postcss = require('gulp-postcss'),
+autoprefixer = require('autoprefixer');
+;
 
 gulp.task('styles', function () {
-	return gulp.src('./app/assets/styles/styles.css').pipe(gulp.dest('./app/temp/styles'));
+	return gulp.src('./app/assets/styles/styles.css')
+	.pipe(postcss([autoprefixer])
+	.pipe(gulp.dest('./app/temp/styles'));
 });
 
 gulp.task('watch', function () {
