aa06ad7ded087e3aae39dc9e4ba299b12bc186df
m-344 <le.chat454@gmail.com>
2019-09-11 17:59:27 +0200

    configured watch task for styles

--- gulpfile.js
+++ gulpfile.js
@@ -2,12 +2,10 @@ var gulp = require('gulp'),
 watch = require('gulp-watch');
 
 gulp.task('styles', function () {
-	console.log('works');
+	return gulp.src('./app/assets/styles/styles.css').pipe(gulp.dest('./app/temp/styles'));
 });
 
 gulp.task('watch', function () {
 	
-	watch('./assets/styles/**/*..css', function () {
-		gulp.start('styles');
-	})
-});
+	watch('./app/assets/styles/**/*.css', gulp.series('styles'));		
+	});
