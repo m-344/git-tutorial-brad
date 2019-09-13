03a7b5fb03941172b4d4273317aaca5f8770775a
m-344 <le.chat454@gmail.com>
2019-09-13 16:59:33 +0200

    update packages

--- gulpfile.js
+++ gulpfile.js
@@ -3,13 +3,13 @@ watch = require('gulp-watch'),
 postcss = require('gulp-postcss'),
 autoprefixer = require('autoprefixer'),
 cssvars = require('postcss-simple-vars'),
-postcssNested = require('postcss-nested');
+nested = require('postcss-nested'),
+cssImport = require('postcss-import');
 
 gulp.task('styles', function () {
-	return gulp.src('./app/assets/styles/styles.css').pipe(postcss([postcssNested, cssvars, autoprefixer])).pipe(gulp.dest('./app/temp/styles'));
+	return gulp.src('./app/assets/styles/styles.css').pipe(postcss([cssImport, nested, cssvars, autoprefixer])).pipe(gulp.dest('./app/temp/styles'));
 });
 
 gulp.task('watch', function () {
-	
 	watch('./app/assets/styles/**/*.css', gulp.series('styles'));
 });
