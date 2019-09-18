44e21d273e7ec4219288fdb5779db27c53b2d7c7
m-344 <le.chat454@gmail.com>
2019-09-18 08:58:16 +0200

    818990 - 13 - Complete Two Blocks

--- gulpfile.js
+++ gulpfile.js
@@ -4,7 +4,8 @@ postcss = require('gulp-postcss'),
 autoprefixer = require('autoprefixer'),
 cssvars = require('postcss-simple-vars'),
 nested = require('postcss-nested'),
-cssImport = require('postcss-import');
+cssImport = require('postcss-import'),
+browserSync = require('browser-sync');
 
 gulp.task('styles', function () {
 	return gulp.src('./app/assets/styles/styles.css').pipe(postcss([cssImport, nested, cssvars, autoprefixer])).pipe(gulp.dest('./app/temp/styles'));
