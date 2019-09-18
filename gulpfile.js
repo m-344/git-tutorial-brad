45be5e14dd1684578526a09f434aa3c50b153fac
m-344 <le.chat454@gmail.com>
2019-09-18 09:02:55 +0200

    beginiing of the lesson 818990 - 14 - What is Browsersync

--- gulpfile.js
+++ gulpfile.js
@@ -5,12 +5,18 @@ autoprefixer = require('autoprefixer'),
 cssvars = require('postcss-simple-vars'),
 nested = require('postcss-nested'),
 cssImport = require('postcss-import'),
-browserSync = require('browser-sync');
+browserSync = require('browser-sync').create();
 
 gulp.task('styles', function () {
 	return gulp.src('./app/assets/styles/styles.css').pipe(postcss([cssImport, nested, cssvars, autoprefixer])).pipe(gulp.dest('./app/temp/styles'));
 });
 
 gulp.task('watch', function () {
+browserSync.init({
+	server: {
+		baseDir: "app"
+	}
+});
+
 	watch('./app/assets/styles/**/*.css', gulp.series('styles'));
 });
