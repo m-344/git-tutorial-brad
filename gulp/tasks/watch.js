4ba60d6180acf812eec77e414bcaaefbe8991d31
OS-50101 <sapov@mozarteum.at>
2019-09-26 17:15:53 +0200

    add option to reload browser after css change

--- gulp/tasks/watch.js
+++ gulp/tasks/watch.js
@@ -16,9 +16,14 @@ gulp.task('watch', function () {
 	});
 	
 	watch('./app/assets/styles/**/*.css', gulp.series('cssInject'));
+	
+	/*	another possibility to update CSS if cssInject doesn't work*/
+	/*	watch('./app/assets/styles/\**\/*.css', function () {
+	browserSync.reload();
+	});*/
 });
 
-/*second argument is calles 'dependencies' (series())*/
+/*second argument is called 'dependencies' (series())*/
 gulp.task('cssInject', gulp.series('styles'), function () {
 	/*return is needed bcs .src is an asynchronious function*/
 	return gulp.src('./app/temp/styles/styles.css').pipe(browserSync.stream());
