9f25e4f4637856f1b6c7eecfa166d745ed723abd
OS-50101 <sapov@mozarteum.at>
2019-10-30 18:15:43 +0100

    create .pipe svgSprite

--- gulp/tasks/sprites.js
+++ gulp/tasks/sprites.js
@@ -1,7 +1,17 @@
 var gulp = require('gulp'),
 svgSprite = require('gulp-svg-sprite');
 
+/*the variable config is an object literal which is passed as an agrument into svgSprite()*/
+var config {
+	mode: {
+		css: {
+			
+		}
+	}
+}
+
 /*arg1='name of the task', arg2='what the task do'*/
 gulp.task('createSprite', function () {
-return gulp.src('./app/assets/images/icons/**/*.svg').pipe(gulp.dest('./app/temp/sprite/'));
+return gulp.src('./app/assets/images/icons/**/*.svg').pipe(svgSprite(config))
+.pipe(gulp.dest('./app/temp/sprite/'));
 });
