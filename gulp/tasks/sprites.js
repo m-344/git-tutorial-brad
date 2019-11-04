c279c82bb77b66f9bf5908eb57c02668832d5357
m-344 <le.chat454@gmail.com>
2019-11-04 19:47:12 +0100

    add span to icon rain

--- gulp/tasks/sprites.js
+++ gulp/tasks/sprites.js
@@ -1,5 +1,6 @@
 var gulp = require('gulp'),
 svgSprite = require('gulp-svg-sprite'),
+del = require('del'),
 rename = require('gulp-rename');
 
 /*the variable config is an object literal which is passed as an agrument into svgSprite()*/
@@ -17,6 +18,10 @@ var config = {
 	}
 };
 
+gulp.task('beginClean', function(){
+	return del(['./app/temp/sprite', './app/assets/images/sprites']);
+});
+
 /*Creates an SVG graphic which merges all input svg files.*/
 /*Arguments of a task method: arg1='the name of the task', arg2='what the task does'*/
 gulp.task('createSprite', function () {
@@ -33,4 +38,8 @@ gulp.task('copySprite', function () {
 	return gulp.src('./app/temp/sprite/css/**/*.css').pipe(rename('_sprite.css')).pipe(gulp.dest('./app/assets/styles/modules/'));
 });
 
-gulp.task('icons', gulp.series('createSprite', 'copySpriteGraphic', 'copySprite'));
+gulp.task('endClean', function(){
+	return del(['./app/temp/sprite']);
+});
+
+gulp.task('icons', gulp.series('beginClean', 'createSprite', 'copySpriteGraphic', 'copySprite', 'endClean'));
