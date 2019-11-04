c729ed6354f73ca97b30e5fa068b9d2c5895d066
OS-50101 <sapov@mozarteum.at>
2019-11-04 18:52:34 +0100

    add docs create 'copySpriteGraphic' task

--- gulp/tasks/sprites.js
+++ gulp/tasks/sprites.js
@@ -6,7 +6,7 @@ rename = require('gulp-rename');
 var config = {
 	mode: {
 		css: {
-			sprite: 'svg/sprite.svg',
+			sprite: 'sprite.svg',
 			render: {
 				css: {
 					/*path to a template*/
@@ -17,16 +17,19 @@ var config = {
 	}
 };
 
-/*arg1='name of the task', arg2='what the task do'*/
+/*Creates an SVG graphic which merges all input svg files.*/
+/*Arguments of a task method: arg1='the name of the task', arg2='what the task does'*/
 gulp.task('createSprite', function () {
 	return gulp.src('./app/assets/images/icons/**/*.svg').pipe(svgSprite(config)).pipe(gulp.dest('./app/temp/sprite/'));
 });
-/*copy file from one destination to another*/
-gulp.task('copySprite', function(){
-	return gulp.src('./app/temp/sprite/css/**/*.css')
-	.pipe(rename('_sprite.css'))
-	.pipe(gulp.dest('./app/assets/styles/modules/'));
+/*Copies the graphic created with createSprite task */
+gulp.task('copySpriteGraphic', function () {
+	return gulp.src('./app/temp/sprite/css/**/*.svg').pipe(gulp.dest('./app/assets/images/sprites'));
 });
 
-gulp.task('icons', gulp.series('createSprite', 'copySprite'));
+/*copies sprite.css to /modules and renames it to _sprite.css*/
+gulp.task('copySprite', function () {
+	return gulp.src('./app/temp/sprite/css/**/*.css').pipe(rename('_sprite.css')).pipe(gulp.dest('./app/assets/styles/modules/'));
+});
 
+gulp.task('icons', gulp.series('createSprite', 'copySpriteGraphic', 'copySprite'));
