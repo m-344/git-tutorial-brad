390032bc0be2c69c6525ff589cc18ba67f129a4a
OS-50101 <sapov@mozarteum.at>
2019-11-11 18:46:41 +0100

    add webpack and webpack-cli and adjusted App.js for a newer version

--- app/assets/scripts/modules/Person.js
+++ app/assets/scripts/modules/Person.js
@@ -8,3 +8,5 @@ function Person(fullName, favColor) {
 		console.log("Hello, my name is " + this.name + " and my favorite color is " + this.favoriteColor + ".");
 	}
 }
+
+module.exports = Person;
--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,9 @@
-{
-	entry: "./app/assets/scripts/App.js",
-	output: {
-		path: "./app/temp/scripts",
-		filename: "App.js"
-	}
-}
+const path = require('path');
+
+module.exports = {
+ entry: "./app/assets/scripts/App.js",
+  output: {
+    filename: 'App.js',
+    path: path.resolve(__dirname, './app/temp/scripts'),
+  },
+};
