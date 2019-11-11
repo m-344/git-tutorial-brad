fac88fa001eac4f708d97b0145549f7d3a8f6226
OS-50101 <sapov@mozarteum.at>
2019-11-11 17:44:36 +0100

    add object Person()

---
+++ app/assets/scripts/App.js
@@ -0,0 +1,16 @@
+/*constructor function
+ * the convention is to begin the blueprint with capital letter
+ * */
+function Person(fullName, favColor) {
+	this.name = fullName;
+	this.favoriteColor = favColor;
+	this.greet = function () {
+		console.log("Hello, my name is " + this.name + " and my favorite color is " + this.favoriteColor + ".");
+	}
+}
+
+var john = new Person("John Doe", "blue");
+john.greet();
+
+var jane = new Person("Jane Smith", "green");
+jane.greet();
