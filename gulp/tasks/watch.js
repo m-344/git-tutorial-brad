4961377c0368d2326381d19fb1db907ebb3f3bba
m-344 <le.chat454@gmail.com>
2019-11-12 18:46:36 +0100

    set up the scriptsRefresh

--- gulp/tasks/watch.js
+++ gulp/tasks/watch.js
@@ -17,19 +17,18 @@ gulp.task('watch', function () {
 	
 	watch('./app/assets/styles/**/*.css', gulp.series('manageCSS'));
 	watch('./app/assets/scripts/**/*.js', gulp.series('scriptsRefresh'));
-/*	watch('./app/assets/scripts/\**\/*.js', gulp.series('scriptsRefresh'));*/
 	
 });
 
 gulp.task('cssInject', function () {
 	return gulp.src('./app/temp/styles/styles.css').pipe(browserSync.stream());
 });
-/*
+
 gulp.task('reloadBrowser', function () {
 	browserSync.reload();	
-});*/
+});
 
 gulp.task('manageCSS', gulp.series('styles', 'cssInject'));
-gulp.task('scriptsRefresh', gulp.series('scripts'));
+gulp.task('scriptsRefresh', gulp.series('scripts', 'reloadBrowser'));
 
 
