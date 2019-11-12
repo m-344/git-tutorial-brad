b8313fc8463fe174927023df5835569e5efcf259
m-344 <le.chat454@gmail.com>
2019-11-12 18:59:27 +0100

    finish lesson 39

--- gulp/tasks/scripts.js
+++ gulp/tasks/scripts.js
@@ -3,8 +3,11 @@ webpack = require('webpack');
 
 
 gulp.task('scripts', function (callback) {
-	webpack(require('../../webpack.config.js'), function() {
-		console.log("test");
+	webpack(require('../../webpack.config.js'), function(err, stats) {
+	if(err){
+		console.log(err.toString() );
+	}
+		console.log(stats.toString());
 		callback();
 	});
 	
