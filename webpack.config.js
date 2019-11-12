de95825af8871f1311e015ad307166e7c719dcc4
m-344 <le.chat454@gmail.com>
2019-11-12 19:32:52 +0100

    add babel

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,18 @@
 const path = require('path');
 
 module.exports = {
- entry: "./app/assets/scripts/App.js",
-  output: {
-    filename: 'App.js',
-    path: path.resolve(__dirname, './app/temp/scripts'),
-  },
+	entry: "./app/assets/scripts/App.js",
+	output: {
+		filename: 'App.js',
+		path: path.resolve(__dirname, './app/temp/scripts'),
+	},
+	module: {
+		loaders:[ {
+			loader: 'babel',
+			query: {
+				presets:[ 'es2015']
+			},
+			test: /\.js$/, /*apply babel for js-files only*/ 				exclude: /node_modules//*says the babel that no js-files have to be processed*/
+		}]
+	}
 };
