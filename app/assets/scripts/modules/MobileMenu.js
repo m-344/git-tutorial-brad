5d93d6a4d2219c007cc570da56c0dddadf14f944
Oleksii Sapov <sapov@mozarteum.at>
2019-11-15 15:24:13 +0100

    add CSS class and rule to the menu icon, finish lesson 40

--- app/assets/scripts/modules/MobileMenu.js
+++ app/assets/scripts/modules/MobileMenu.js
@@ -3,13 +3,20 @@ import $ from 'jquery';
 
 class MobileMenu {
 	constructor() {
+		// selecting elements from the DOM
 		this.menuIcon = $(".site-header__menu-icon");
 		this.menuContent = $(".site-header__menu-content");
+		// call the function
 		this.events();
 	}
+	// event handling method: when menuIcon is cklicked we want to call the toggleTheMenu()
 	events() {
-		this.menuIcon.click(this.toggleTheMenu);
+		// use bind() to pass the object whithin the keyword 'this' to the toggleTheMenu()
+	 // Otherwise the 'this' in that function will have the value of the clicked DOM element
+		this.menuIcon.click(this.toggleTheMenu.bind(this));
+
 	}
+	// defining functionality
 	toggleTheMenu() {
 		this.menuContent.toggleClass("site-header__menu-content--is-visible");
 	}
