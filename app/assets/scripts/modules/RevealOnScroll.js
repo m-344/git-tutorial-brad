4b609cbb90efc20743c92172364a7b83a00bd1ed
Oleksii Sapov <sapov@mozarteum.at>
2019-11-22 19:03:43 +0100

    up to min 10:52

--- app/assets/scripts/modules/RevealOnScroll.js
+++ app/assets/scripts/modules/RevealOnScroll.js
@@ -1,6 +1,13 @@
+import $ from 'jquery';
+
 class RevealOnScroll {
   constructor() {
-
+    this.itemsToReveal = $(".feature-item");
+    this.hideInitially();
+  }
+  // a new method
+  hideInitially() {
+    this.itemsToReveal.addClass("reveal-item");
   }
 }
 
