34390b4fe55ac4f4efc549206d3fbf812e0d6ecc
m-344 <le.chat454@gmail.com>
2019-11-26 18:47:12 +0100

    add new css reveal item

--- app/assets/scripts/modules/RevealOnScroll.js
+++ app/assets/scripts/modules/RevealOnScroll.js
@@ -5,6 +5,14 @@ class RevealOnScroll {
   constructor() {
     // select the els using jquery
     this.itemsToReveal = $(".feature-item");
+    // run the method which is defined below
+    this.hideInitially();
+  }
+
+  // as soon as the page loads the class will be added
+  // using jquery method
+  hideInitially() {
+    this.itemsToReveal.addClass("reveal-item");
   }
 }
 
