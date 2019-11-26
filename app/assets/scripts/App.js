646b136c4f2acfcdef4b2cf9965ae0f97609ea9c
m-344 <le.chat454@gmail.com>
2019-11-26 18:43:32 +0100

    started develop feature for revealing

--- app/assets/scripts/App.js
+++ app/assets/scripts/App.js
@@ -1,3 +1,6 @@
 import MobileMenu from './modules/MobileMenu';
+import RevealOnScroll from './modules/RevealOnScroll';
 
+// create an object that is an instance of the class (or so-called blueprint)...
 var mobileMenu = new MobileMenu();
+var revealOnScroll = new RevealOnScroll();
---
+++ app/assets/scripts/modules/RevealOnScroll.js
@@ -0,0 +1,11 @@
+import $ from 'jquery';
+
+// hide the elements
+class RevealOnScroll {
+  constructor() {
+    // select the els using jquery
+    this.itemsToReveal = $(".feature-item");
+  }
+}
+
+export default RevealOnScroll;
