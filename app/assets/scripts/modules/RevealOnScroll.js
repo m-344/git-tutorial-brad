b78572daca65ce4e43226d0a29be5ac5b06cd12d
m-344 <le.chat454@gmail.com>
2019-11-29 09:51:53 +0100

    make the class RevealOnScroll dynamic, added two arguments

--- app/assets/scripts/modules/RevealOnScroll.js
+++ app/assets/scripts/modules/RevealOnScroll.js
@@ -1,18 +1,19 @@
 import $ from 'jquery';
 import waypoints from '../../../../node_modules/waypoints/lib/noframework.waypoints';
 
-// hide the elements
+// hide or show the elements
 class RevealOnScroll {
-  constructor() {
+  constructor(els, offset) {
     // select the els using jquery = collection of the elements
-    this.itemsToReveal = $(".feature-item");
-    // run the method which is declared outside the constructor function
+    this.itemsToReveal = els;
+    // first hide the elementes = run the method which is declared outside the constructor function
+    this.offsetPercentage = offset;
     this.hideInitially();
-    // call the method createWaypoints
+    // show the elements when needed = call the method createWaypoints
     this.createWaypoints();
   }
 
-  // as soon as the page loads the class will be added
+  // as soon as the page loads, the class will be added
   // using jquery method
   hideInitially() {
     this.itemsToReveal.addClass("reveal-item");
@@ -20,17 +21,20 @@ class RevealOnScroll {
 
   createWaypoints() {
     // use jquery'each' = loop for every item in the collection itemsToReveal
+    var that = this;
     this.itemsToReveal.each(function() {
+      // the variable is needed to store the current item
       var currentItem = this;
       // create a new object that is an instance of the Waypoint class (avaiable from the modul waypoints)
       new Waypoint({
-        // DOM element to watch for when scrolling down the page
+        // DOM element to watch for, when scrolling down the page
+        // using $currentItem bcs otherwise 'this' would refere to something else
         element: currentItem,
         // what to do when the element is scrolled to
         handler: function() {
           $(currentItem).addClass("reveal-item--is-visible");
         },
-        offset: "85%"
+        offset: that.offsetPercentage
       })
     });
   }
