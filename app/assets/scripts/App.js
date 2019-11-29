1f2096d0c1f1dfad5253b0608986100409ef72cf
m-344 <le.chat454@gmail.com>
2019-11-29 12:40:59 +0100

    work in progress

--- app/assets/scripts/App.js
+++ app/assets/scripts/App.js
@@ -1,8 +1,10 @@
 import MobileMenu from './modules/MobileMenu';
 import RevealOnScroll from './modules/RevealOnScroll';
 import $ from 'jquery';
+import RevealOnScroll from './modules/StickyHeader';
 
 // create an object that is an instance of the class (or so-called blueprint)...
 var mobileMenu = new MobileMenu();
 new RevealOnScroll($(".feature-item"), "85%");
 new RevealOnScroll($(".testimonial"), "60%");
+var stickyHeader = new StickyHeader();
---
+++ app/assets/scripts/modules/StickyHeader.js
@@ -0,0 +1,10 @@
+import $ from 'jquery';
+import waypoints from '../../../../node_modules/waypoints/lib/noframework.waypoints';
+class ClassName {
+  constructor() {
+    this.siteHeader = $(".site-header");
+  }
+}
+
+// export is needed in order to be abel touse it in another file
+export default StickyHeader;
