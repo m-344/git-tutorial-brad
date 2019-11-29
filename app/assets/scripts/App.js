0f63641fdc5f49c9b5a2d252af4d40e1b64b87c5
m-344 <le.chat454@gmail.com>
2019-11-29 09:41:17 +0100

    add two more instances of RevealOnScroll

--- app/assets/scripts/App.js
+++ app/assets/scripts/App.js
@@ -1,6 +1,8 @@
 import MobileMenu from './modules/MobileMenu';
 import RevealOnScroll from './modules/RevealOnScroll';
+import $ from 'jquery';
 
 // create an object that is an instance of the class (or so-called blueprint)...
 var mobileMenu = new MobileMenu();
-var revealOnScroll = new RevealOnScroll();
+new RevealOnScroll($(".feature-item"), "85%");
+new RevealOnScroll($(".testimonial"), "60%");
