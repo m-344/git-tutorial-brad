b532d097d9f5e7b45a7b5ecd06586586c9387c4a
Oleksii Sapov <sapov@mozarteum.at>
2019-12-10 18:44:13 +0100

    add Modal.js

--- app/assets/scripts/App.js
+++ app/assets/scripts/App.js
@@ -2,9 +2,11 @@ import MobileMenu from './modules/MobileMenu';
 import RevealOnScroll from './modules/RevealOnScroll';
 import $ from 'jquery';
 import StickyHeader from './modules/StickyHeader';
+import Modal from './modules/Modal';
 
 // create an object that is an instance of the class (or so-called blueprint)...
 var mobileMenu = new MobileMenu();
 new RevealOnScroll($(".feature-item"), "85%");
 new RevealOnScroll($(".testimonial"), "60%");
 var stickyHeader = new StickyHeader();
+var modal = new Modal();
