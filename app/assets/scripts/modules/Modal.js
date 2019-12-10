22ceb0aba1dc4205299570124d5ec87ddc91a352
Oleksii Sapov <sapov@mozarteum.at>
2019-12-10 19:00:04 +0100

    implement events() method

--- app/assets/scripts/modules/Modal.js
+++ app/assets/scripts/modules/Modal.js
@@ -1,6 +1,34 @@
+import $ from 'jquery';
+
 class Modal {
   constructor() {
+    this.openModalButton = $(".open-modal");
+    this.modal = $(".modal");
+    this.closeModalButton = $(".modal-close");
+    this.events();
+  }
 
+  events() {
+    // clicking the open modal button
+    // when the element with the class 'open-modal' is clicked
+    // the method openModal is run
+    this.openModalButton.click(this.openModal.bind(this));
+
+    // clicking the x close button
+    // the .bind method sets the value of the 'this'-keyword
+    this.closeModalButton.click(this.closeModal.bind(this));
+
+    // pushes the escape key
+  }
+
+  openModal() {
+    this.modal.addClass("modal--is-visible");
+    // prevents browser the default behaviour of scrolling up
+    return false;
+  }
+
+  closeModal() {
+    this.modal.removeClass("modal--is-visible");
   }
 }
 
