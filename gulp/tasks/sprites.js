ba50b83f81edb31a0c9a97612803a8e53bbd7b10
Oleksii Sapov <sapov@mozarteum.at>
2019-12-12 18:56:25 +0100

    min 07:59

--- gulp/tasks/sprites.js
+++ gulp/tasks/sprites.js
@@ -1,45 +1,57 @@
 var gulp = require('gulp'),
-svgSprite = require('gulp-svg-sprite'),
-del = require('del'),
-rename = require('gulp-rename');
+  svgSprite = require('gulp-svg-sprite'),
+  del = require('del'),
+  rename = require('gulp-rename'),
+  svg2png = require('gulp-svg2png');
 
 /*the variable config is an object literal which is passed as an agrument into svgSprite()*/
 var config = {
-	mode: {
-		css: {
-			sprite: 'sprite.svg',
-			render: {
-				css: {
-					/*path to a template*/
-					template: './gulp/templates/sprite.css'
-				}
-			}
-		}
-	}
+  mode: {
+    css: {
+      sprite: 'sprite.svg',
+      render: {
+        css: {
+          /*path to a template*/
+          template: './gulp/templates/sprite.css'
+        }
+      }
+    }
+  }
 };
 
-gulp.task('beginClean', function(){
-	return del(['./app/temp/sprite', './app/assets/images/sprites']);
+gulp.task('beginClean', function() {
+  return del(['./app/temp/sprite', './app/assets/images/sprites']);
 });
 
 /*Creates an SVG graphic which merges all input svg files.*/
 /*Arguments of a task method: arg1='the name of the task', arg2='what the task does'*/
-gulp.task('createSprite', function () {
-	return gulp.src('./app/assets/images/icons/**/*.svg').pipe(svgSprite(config)).pipe(gulp.dest('./app/temp/sprite/'));
+gulp.task('createSprite', function() {
+  return gulp.src('./app/assets/images/icons/**/*.svg')
+    .pipe(svgSprite(config))
+    .pipe(gulp.dest('./app/temp/sprite/'));
+});
+
+gulp.task('createPngCopy', function() {
+  return gulp.src('./app/temp/sprite/css/*.svg')
+    .pipe(svg2pgn())
+    .pipe(gulp.dest('.app/temp/sprite/css'));
 });
 
 /*Copies the graphic which was created by  the 'createSprite' task */
-gulp.task('copySpriteGraphic', function () {
-	return gulp.src('./app/temp/sprite/css/**/*.svg').pipe(gulp.dest('./app/assets/images/sprites'));
+gulp.task('copySpriteGraphic', function() {
+  return gulp.src('./app/temp/sprite/css/**/*.{svg,png}')
+    .pipe(gulp.dest('./app/assets/images/sprites'));
 });
 
 /*copies sprite.css to /modules and renames it to _sprite.css*/
-gulp.task('copySprite', function () {
-	return gulp.src('./app/temp/sprite/css/**/*.css').pipe(rename('_sprite.css')).pipe(gulp.dest('./app/assets/styles/modules/'));
+gulp.task('copySprite', function() {
+  return gulp.src('./app/temp/sprite/css/**/*.css')
+    .pipe(rename('_sprite.css'))
+    .pipe(gulp.dest('./app/assets/styles/modules/'));
 });
 
-gulp.task('endClean', function(){
-	return del(['./app/temp/sprite']);
+gulp.task('endClean', function() {
+  return del(['./app/temp/sprite']);
 });
 
-gulp.task('icons', gulp.series('beginClean', 'createSprite', 'copySpriteGraphic', 'copySprite', 'endClean'));
+gulp.task('icons', gulp.series('beginClean', 'createSprite', 'createPngCopy', 'copySpriteGraphic', 'copySprite', 'endClean'));
