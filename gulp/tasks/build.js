cc877d009438aaaf77c1bd3385bd011717302eb9
Oleksii Sapov <sapov@mozarteum.at>
2019-12-18 12:16:09 +0100

    add previewDist

--- gulp/tasks/build.js
+++ gulp/tasks/build.js
@@ -4,7 +4,18 @@ var gulp = require('gulp'),
   usemin = require('gulp-usemin'),
   rev = require('gulp-rev'),
   cssnano = require('gulp-cssnano'),
-  uglify = require('gulp-uglify');
+  uglify = require('gulp-uglify'),
+  browserSync = require('browser-sync').create();
+
+
+gulp.task('previewDist', function() {
+  browserSync.init({
+    notify: false,
+    server: {
+      baseDir: "dist"
+    }
+  });
+})
 
 gulp.task('deleteDistFolder', function() {
   return del("./dist");
